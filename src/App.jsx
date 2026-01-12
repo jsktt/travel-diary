74cc8b3b8782051422252a415c71b8d1e12d6e11
Junsung Kim <uitomde@gmail.com>
2026-01-12 16:13:04 +0900

    did testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import TravelList from './components/TravelList';
@@ -7,6 +7,7 @@ import TravelForm from './components/TravelForm';
 
 function App() {
 
+  
   const [travels, setTravels] = useState(() => {
     const saved = localStorage.getItem(`travels`);
     if (saved) {
