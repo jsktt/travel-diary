c0355943926e4c88d53fc161d8a8af35825de68f
Junsung Kim <uitomde@gmail.com>
2026-01-12 14:27:37 +0900

    framaework

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <div className=
+      "App">
+        
+      <h1>여행 기록</h1>
+    </div>
+  );
 }
-
-export default App
+export default App;
