c4875ac5db0f7ee55638adc354a293f5993c33d9
idayoung <dayounglee0311@gmail.com>
2026-01-12 15:04:59 +0900

    dayoung- initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import TravelForm from './components/TravelForm';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       "App">
         
       <h1>여행 기록</h1>
+      <TravelForm/>
     </div>
   );
 }
